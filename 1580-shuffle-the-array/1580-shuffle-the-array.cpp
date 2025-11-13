4ff6f914f1af75b44dcc81e396922f64c9d7434b
Trần Đức Bảo <24020039@vnu.edu.vn>
2025-11-13 10:27:25 +0700

    Time: 4 ms (64.36%), Space: 13.5 MB (51.92%) - LeetHub

--- 1580-shuffle-the-array/1580-shuffle-the-array.cpp
+++ 1580-shuffle-the-array/1580-shuffle-the-array.cpp
@@ -2,9 +2,10 @@ class Solution {
 public:
     vector<int> shuffle(vector<int>& nums, int n) {
         vector<int> ans;
+        ans.resize(2 * n);
         for(size_t i = 0; i < n; ++i) {
-            ans.push_back(nums[i]);
-            ans.push_back(nums[i + n]);
+            ans[i * 2] = nums[i];
+            ans[i * 2 + 1] = nums[i + n];
         }
         return ans;
     }
