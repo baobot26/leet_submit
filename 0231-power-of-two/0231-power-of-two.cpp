033279f5e8fea17467d53c34a8b8bb5b91c5c526
Trần Đức Bảo <24020039@vnu.edu.vn>
2025-11-16 14:18:27 +0700

    Time: 0 ms (100%), Space: 7.8 MB (82.55%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -1,20 +1,6 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        if (n <= 0) {
-            return false;
-        }
-        int res = 0;
-        while (n != 0) {
-            int temp = n % 2;
-            if (temp) {
-                res++;
-            }
-            n /= 2;
-        }
-        if (res == 1) {
-            return true;
-        }
-        return false;
+        return (n > 0) && (n & (n - 1)) == 0;
     }
 };
